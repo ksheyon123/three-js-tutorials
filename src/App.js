6987ed1c5366e2e53dc7ad3503ad4f33837f930c
Kangseohyun <kshyeon123@gmail.com>
2023-02-07 09:42:14 +0900

    Before adding css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import styled, { ThemeProvider, createGlobalStyle } from "styled-components";
+
+const GlobalStyling = createGlobalStyle`
+ body {
+   margin : 0px;
+ }
+`;
 
 const App = () => {
-  return <div>Application</div>;
+  return (
+    <ThemeProvider theme={{}}>
+      <GlobalStyling />
+      <div>Application</div>
+    </ThemeProvider>
+  );
 };
 
 export default App;
