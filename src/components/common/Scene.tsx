c7e02bbd9a6c26a08b92d5376d9d0e2129baa464
kangseohyeon <kshyeon123@gmail.com>
2024-04-25 16:27:09 +0900

    :sparkles:feat > understanding object direction

--- src/components/common/Scene.tsx
+++ src/components/common/Scene.tsx
@@ -14,20 +14,42 @@ export const Scene: React.FC = () => {
       0.1,
       1000
     );
+    const axisHelper = new THREE.AxesHelper();
+    scene.add(axisHelper);
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     // document.body.appendChild( renderer.domElement );
     // use ref as a mount point of the Three.js scene instead of the document.body
     canvasRef.current && canvasRef.current.appendChild(renderer.domElement);
-    var geometry = new THREE.BoxGeometry(1, 1, 1);
+    var geometry = new THREE.ConeGeometry(0.5, 1.5, 32);
     var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
     var cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
-    camera.position.z = 20;
-    camera.position.x = 10;
+    camera.position.set(0, 0, 5);
+    // console.log(camera);
+
+    console.log(camera.quaternion);
+    const forward = new THREE.Vector3(0, 0, -10).applyQuaternion(
+      camera.quaternion
+    );
+    console.log(new THREE.Vector3().copy(camera.position).add(forward));
+    camera.lookAt(new THREE.Vector3().copy(camera.position).add(forward));
+    // For cube
+    // cube.matrixAutoUpdate = false;
+    // const position = new THREE.Vector3(1, 0, 1);
+    // cube.matrix.setPosition(position);
+    // const eye = position.clone();
+    // const target = new THREE.Vector3(0, 1, 0);
+    // const up = new THREE.Vector3(0, 0, 1);
+    // cube.matrix.lookAt(eye, target, up);
+
+    // For camera
+    // camera.matrixAutoUpdate = false;
+    // camera.matrix.setPosition(position);
+    // camera.matrix.lookAt(eye, target, up);
 
     const controls = new OrbitControls(camera, renderer.domElement);
-    controls.update();
+    // controls.update();
     var animate = function () {
       requestAnimationFrame(animate);
       controls.update();
